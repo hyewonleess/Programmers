74b8bf2f5e4c49a9015a6b8ebac4fa53fe19010e
HYEWON LEE <hyewon328@naver.com>
2021-10-06 23:41:58 +0900

    Update 모의고사.py

--- 고득점Kit/Level1/모의고사.py
+++ 고득점Kit/Level1/모의고사.py
@@ -18,3 +18,26 @@ def solution(answers):
         
     answer = [i+1 for i, x in enumerate(cnts) if x == max(cnts)]
     return answer
+
+
+# best solution
+def solution(answers):
+    p1 = [1,2,3,4,5]
+    p2 = [2,1,2,3,2,4,2,5]
+    p3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, ans in enumerate(answers):
+        if ans == p1[idx%len(p1)]: # 나머지를 효율적으로 이용한 풀이
+            score[0] += 1
+        if ans == p2[idx%len(p2)]:
+            score[1] += 1
+        if ans == p3[idx%len(p3)]:
+            score[2] += 1
+    
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+    
+    return result
